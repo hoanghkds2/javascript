407c4baed1dd702114edc32739e29048ad7f6d58
hoanghkds2 <130424886+hoanghkds2@users.noreply.github.com>
2023-12-25 22:06:27 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,71 +1,124 @@
-function Student(name, year, toan, ly, hoa) {
+function Student(name, year, math, physics, chemistry) {
     this.name = name;
     this.year = year;
-    this.toan = parseFloat(toan);
-    this.ly = parseFloat(ly);
-    this.hoa = parseFloat(hoa);
-    this.dtb = 0;
-    this.hl = "None";
-}
-
-var students = [];
-
-function Add() {
-    var name = $("#name").val();
-    var year = $("#year").val();
-    var toan = $("#toan").val();
-    var ly = $("#ly").val();
-    var hoa = $("#hoa").val();
-
-    var newStudent = new Student(name, year, toan, ly, hoa);
+    this.math = parseFloat(math);
+    this.physics = parseFloat(physics);
+    this.chemistry = parseFloat(chemistry);
+    this.average = 0;
+    this.type = "None";
+  }
+  
+  let students = [];
+  
+  $(document).ready(function () {
+    $("form").submit(function (event) {
+      event.preventDefault(); 
+  
+      if (validateForm()) {
+        Add();
+        clearForm();
+      }
+    });
+  
+    $("#averageButton").click(countAverage);
+    $("#hocLucButton").click(hocLuc);
+  });
+  
+  function validateForm() {
+    let name = $("#name").val();
+    let toan = parseFloat($("#toan").val());
+    let ly = parseFloat($("#ly").val());
+    let hoa = parseFloat($("#hoa").val());
+  
+    if (name.trim() === "") {
+      alert("Vui lòng nhập tên.");
+      return false;
+    }
+  
+    if (isNaN(toan) || toan < 1 || toan > 10) {
+      alert("Điểm Toán phải là một số từ 1 đến 10.");
+      return false;
+    }
+  
+    if (isNaN(ly) || ly < 1 || ly > 10) {
+      alert("Điểm Lý phải là một số từ 1 đến 10.");
+      return false;
+    }
+  
+    if (isNaN(hoa) || hoa < 1 || hoa > 10) {
+      alert("Điểm Hóa phải là một số từ 1 đến 10.");
+      return false;
+    }
+  
+    return true;
+  }
+  
+  function Add() {
+    let name = $("#name").val();
+    let year = $("#year").val();
+    let math = $("#toan").val();
+    let physics = $("#ly").val();
+    let chemistry = $("#hoa").val();
+  
+    let newStudent = new Student(name, year, math, physics, chemistry);
     students.push(newStudent);
     in_DSSV();
-}
-
-function in_DSSV() {
-    var table = $("#mytable");
+    
+    return false;
+  }
+  
+  function in_DSSV() {
+    let table = $("#mytable");
     table.empty();
     
     $.each(students, function(i, student) {
-        var tr = $("<tr>").attr("id", i); 
-        table.append(tr);
-
-        var tname = $("<td>").text(student.name);
-        var tyear = $("<td>").text(student.year);
-        var ttoan = $("<td>").text(student.toan);
-        var tly = $("<td>").text(student.ly);
-        var thoa = $("<td>").text(student.hoa);
-        var tdtb = $("<td>").text(student.dtb);
-        var thl = $("<td>").text(student.hl);
-
-        tr.append(tname);
-        tr.append(tyear);
-        tr.append(ttoan);
-        tr.append(tly);
-        tr.append(thoa);
-        tr.append(tdtb);
-        tr.append(thl);
+      var tr = $("<tr>").attr("id", i); 
+      table.append(tr);
+  
+      let tname = $("<td>").text(student.name);
+      let tyear = $("<td>").text(student.year);
+      let tmath = $("<td>").text(student.math);
+      let tphysics = $("<td>").text(student.physics);
+      let tchemistry = $("<td>").text(student.chemistry);
+      let taverage = $("<td>").text(student.average);
+      let ttype = $("<td>").text(student.type);
+  
+      tr.append(tname);
+      tr.append(tyear);
+      tr.append(tmath);
+      tr.append(tphysics);
+      tr.append(tchemistry);
+      tr.append(taverage);
+      tr.append(ttype);
     });
-}
-
-function Average() {
-    $.each(students, function(i, student) {
-        student.dtb = (student.toan + student.ly + student.hoa) / 3;
-    });
-    in_DSSV();
-}
-
-function hocLuc() {
-    $.each(students, function(i, student) {
-        if (student.dtb >= 8) {
-            student.hl = "Giỏi";
-        } else if (student.dtb < 8 && student.dtb > 5) {
-            student.hl = "Khá";
-        } else if (student.dtb < 5 && student.dtb > 3) {
-            student.hl = "Trung Bình";
-        } else {
-            student.hl = "Yếu";
-        }
+  }
+  
+  function countAverage() {
+    $.each(students, function(_i, student) {
+      student.average = (student.math + student.physics + student.chemistry) / 3;
     });
     in_DSSV();
-}
+  }
+  
+  function hocLuc() {
+    $.each(students, function(_i, student) {
+      if (student.average >= 8) {
+        student.type = "Giỏi";
+      } else if (student.average >= 5) {
+        student.type = "Khá";
+      } else if (student.average >= 3) {
+        student.type = "Trung Bình";
+      } else {
+        student.type = "Yếu";
+      }
+    });
+    in_DSSV();
+  }
+  
+  function clearForm() {
+    $("#name").val("");
+    $("#year").val("");
+    $("#toan").val("");
+    $("#ly").val("");
+    $("#hoa").val("");
+  }
